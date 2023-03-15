b6c88ce533e01e1ac9ef9bca7102193448e2fc5b
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-03-15 00:40:31 -1000

    progress with color scheme, bg-color bug buggin

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
-import resume from "../pages/about/jacqueline-cashman-resume.pdf";
+// import resume from "../pages/about/jacqueline-cashman-resume.pdf";
 
 const AboutMe = ({ name, email, location, availability, brand }) => {
   const [ref, inView] = useInView({
@@ -20,7 +20,7 @@ const AboutMe = ({ name, email, location, availability, brand }) => {
   const handleDownload = () => {
     setDownloading(true);
     const link = document.createElement("a");
-    link.href = resume;
+    // link.href = resume;
     link.download = "Jacqueline-Cashman-Resume.pdf";
     link.onload = () => {
       link.remove();
